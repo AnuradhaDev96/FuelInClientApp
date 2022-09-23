22ed744b4e624f61d1f0240626bd9e7fa9ef8082
AnuradhaDev96 <anusampath9470@gmail.com>
2022-09-23 08:04:26 +0530

    Code text color is fixed create dialogs

--- lib/src/ui/admin_ui/administrative_divisions/create_grama_niladari_division_dialog.dart
+++ lib/src/ui/admin_ui/administrative_divisions/create_grama_niladari_division_dialog.dart
@@ -1,9 +1,5 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:matara_division_system/src/models/administrative_units/divisional_secretariats.dart';
-
 import '../../../config/app_colors.dart';
 import '../../../config/language_settings.dart';
 import '../../../models/administrative_units/grama_niladari_divisions.dart';
@@ -38,6 +34,7 @@ class CreateGramaNiladariDivisionDialog extends StatelessWidget {
                   builder: (context, snapshot, child) {
                     return RichText(
                       text: TextSpan(
+                        style: const TextStyle(color: AppColors.black),
                         children: [
                           const TextSpan(
                             text: "fla;h( ", //කේතය:
